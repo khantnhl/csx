0da46cc0c33ce26465e1c66ed897ab14966c8d0e
Hlaing, Khant Nyi <knyihlaing@go.pasadena.edu>
2025-03-19 01:02:03 -0700

    register error

--- src/main/java/com/csx/app/config/SecurityConfig.java
+++ src/main/java/com/csx/app/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf-> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/api/auth/**").permitAll()
+                    .requestMatchers("/api/v1/auth/**").permitAll()
                     .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
